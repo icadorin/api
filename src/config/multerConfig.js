3654d6dd2a1e73688367e6d71fe3c83aee74774e
israelmurilocadorin <israel.del96@gmail.com>
2023-04-25 17:41:23 -0300

    bug fix url

--- src/config/multerConfig.js
+++ src/config/multerConfig.js
@@ -4,14 +4,14 @@ import { extname, resolve } from 'path';
 const random = () => Math.floor(Math.random() * 10000 + 10000);
 
 export default {
-  storage: multer.diskStorage({
-    fileFilter: (req, file, cb) => {
-      if (file.mimetype !== 'image/png' && file.mimetype !== 'image/jpeg') {
-        return cb(new multer.MulterError('Arquivo precisa ser PNG ou JPG.'));
-      }
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype !== 'image/png' && file.mimetype !== 'image/jpeg') {
+      return cb(new multer.MulterError('Arquivo precisa ser PNG ou JPG.'));
+    }
 
-      return cb(null, true);
-    },
+    return cb(null, true);
+  },
+  storage: multer.diskStorage({
     destination: (req, file, cb) => {
       cb(null, resolve(__dirname, '..', '..', 'uploads', 'images'));
     },
